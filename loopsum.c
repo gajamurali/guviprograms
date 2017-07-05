1ef22f09600a563400627f76df379a6e7f02a6b9
gajamurali <mgajalakshmi2949@gmail.com>
2017-07-05 21:34:40 +0530

    Add files via upload

---
+++ loopsum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,sum=0,i;
+clrscr();
+scanf("%d",&n);
+if(n>0)
+{
+for(i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+printf("sum of %d natural numbers: %d",n,sum);
+}
+else
+{
+printf("wrong entry");
+}
+getch();
+}
