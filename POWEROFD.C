07ecdda1653a39b8183b1663444e42cc5d3621d8
gajamurali <mgajalakshmi2949@gmail.com>
2017-09-07 00:11:43 +0530

    Add files via upload

---
+++ POWEROFD.C
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+int no,i=0,r,a[50],len,sum=0,j,power=1;
+clrscr();
+scanf("%d",&no);
+if(no>9)
+{
+for(i=0;no!=0;i++)
+{
+r=no%10;
+no=no/10;
+a[i]=r;
+len=i;
+}
+for(i=0;i<=len;i++)
+{
+for(j=a[0];j!=0;j--)
+{
+power=power*a[i];
+}
+sum=sum+power;
+power=1;
+}
+}
+printf("%d",sum);
+getch();
+return 0;
+}
